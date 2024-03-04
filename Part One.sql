bcb8feb804e2cb46a36bd85b88402f2f5478e175
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-03-04 15:28:06 +0500

    PART Two Start

--- Part One.sql
+++ Part One.sql
@@ -1,5 +1,40 @@
 show databases;
 
-use sql_hr;
+use sql_store;
 
 show tables;
+
+-- SELECT , FROM,  WHERE ORDER BY  - 
+
+SELECT * FROM customers;
+
+
+SELECT * 
+FROM customers 
+where customer_id = 1  
+ORDER BY first_name;
+
+-- * + - / can be use According to USE 
+-- We can use () to Execute Operation like (points + 10) * 100 
+-- Alias for Naming to show the Columns like dis_fact , "dis fact"
+  
+SELECT 	last_name , 
+		first_name , 
+        points , 
+        points * 10 + 100 AS 'discount factor'
+FROM customers;
+
+
+SELECT DISTINCT state 
+FROM customers;
+
+-- Exercise to increase Price of the Product
+ 
+SELECT * from products;
+
+SELECT 	name ,
+		unit_price ,
+		unit_price * 1.1 AS "New Price"
+FROM products;
+
+
