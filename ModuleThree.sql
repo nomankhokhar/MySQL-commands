da75afa334e752982e4f38f7d064121df67de1e5
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-03-05 23:38:43 +0500

    Joining Multiple Table

--- ModuleThree.sql
+++ ModuleThree.sql
@@ -33,11 +33,37 @@ SELECT order_id, oi.product_id, quantity, oi.unit_price
 FROM order_items oi
 JOIN products p ON oi.product_id = p.product_id;
  
+
 -- Joining Acros Databases
+-- First use the database then use non running database for the joining across the database
  
 USE sql_store;
- 
+
 SELECT *
 FROM order_items oi
 JOIN sql_inventory.products p
 	ON oi.product_id = p.product_id;
+
+
+-- Self Join Table
+
+USE sql_hr;
+
+-- Check Weather Employee is Manager or not 
+SELECT *
+FROM employees e
+JOIN employees m
+	ON e.reports_to = m.employee_id;
+
+
+SELECT e.employee_id, 
+	   e.first_name,
+       m.first_name as manager
+FROM employees e
+JOIN employees m
+	ON e.reports_to = m.employee_id;
+
+
+-- Joining Multiple Tables
+
+ 
