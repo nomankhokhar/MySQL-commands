a983faccc239ffed3b19112c44ff08d9792bbf83
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-03-05 23:25:44 +0500

    Module Video 2 Done

--- ModuleThree.sql
+++ ModuleThree.sql
@@ -9,7 +9,35 @@ JOIN customers
     
     
     
-SELECT order_id , first_name , last_name
+SELECT order_id , orders.customer_id ,first_name , last_name
 FROM orders
 JOIN customers
 	ON orders.customer_id = customers.customer_id;
+    
+-- Repetion Remove with Alias
+
+SELECT order_id , o.customer_id ,first_name , last_name
+FROM orders o
+JOIN customers c
+	ON o.customer_id = c.customer_id;
+    
+-- Exercise show Order item that are in Product Table
+
+SELECT *
+FROM order_items oi
+JOIN products p ON oi.product_id = p.product_id;
+
+-- Exercise   
+
+SELECT order_id, oi.product_id, quantity, oi.unit_price
+FROM order_items oi
+JOIN products p ON oi.product_id = p.product_id;
+ 
+-- Joining Acros Databases
+ 
+USE sql_store;
+ 
+SELECT *
+FROM order_items oi
+JOIN sql_inventory.products p
+	ON oi.product_id = p.product_id;
