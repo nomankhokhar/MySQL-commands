34b022d1940be714f25f9f26a7ac252aafdff47b
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-03-06 23:14:29 +0500

    9 Self Outer Joins

--- ModuleThree.sql
+++ ModuleThree.sql
@@ -69,3 +69,125 @@ JOIN employees m
 SELECT *
 FROM sql_store.orders;
 
+
+SELECT *
+FROM sql_store.order_statuses;
+
+USE sql_store;
+
+-- Fetch all Data from Tables
+
+SELECT *
+FROM orders o
+JOIN customers c
+	ON o.customer_id = c.customer_id
+JOIN order_statuses os
+	ON o.status = os.order_status_id;
+    
+-- Fetch only Specific Tables
+-- Show Order Status with Name and ID
+
+SELECT o.order_id,
+	   o.order_date,
+       c.first_name,
+       c.last_name,
+       os.name AS statis
+FROM orders o
+JOIN customers c
+	ON o.customer_id = c.customer_id
+JOIN order_statuses os
+	ON o.status = os.order_status_id; 
+    
+-- Exercise Find name and Payment Methods of the Clients
+
+USE sql_invoicing;
+
+SELECT *
+FROM payments p
+JOIN clients c
+	ON p.client_id = c.client_id
+JOIN payment_methods pm
+	ON p.payment_method = pm.payment_method_id;
+    
+    
+-- Compount Join Conditions when One table 
+-- have more than one Primary key we use
+-- Compound Key to Fetch Data  
+
+USE sql_store;
+
+SELECT *
+FROM order_items oi
+JOIN order_item_notes oin
+	ON oi.order_id = oin.order_id
+    AND oi.product_id = oin.product_id; 
+    
+    
+-- Implicit Join Syntax
+-- Writing Query withoud JOIN KeyWord
+ 
+ -- Simple Way
+ SELECT *
+ FROM orders o
+ JOIN customers c
+	ON o.customer_id = c.customer_id;
+ 
+-- Implicit way of writing the Query
+SELECT *
+FROM orders o, customers c
+WHERE o.customer_id = c.customer_id;
+
+
+-- Outer Join Table
+-- LEFT and RIGHT are the Outer Joins
+ 
+USE sql_store;
+
+SELECT
+	c.customer_id,
+    c.first_name,
+    o.order_id
+FROM customers c
+LEFT JOIN orders o
+	ON c.customer_id = o.customer_id
+ORDER BY c.customer_id;
+
+
+-- Outer Join Between Multiple Tables
+
+SELECT
+	c.customer_id,
+    c.first_name,
+    o.order_id,
+    sh.name AS shipper
+FROM customers c
+LEFT JOIN orders o
+	ON c.customer_id = o.customer_id
+LEFT JOIN shippers sh
+	ON o.shipper_id = sh.shipper_id
+ORDER BY c.customer_id;
+
+
+-- Exercise that fine order_date, order_id,
+-- first_name. shipper, status
+
+SELECT
+	o.order_id,
+    o.order_date,
+    c.first_name AS customer,
+    sh.name AS shipper,
+    os.name AS status
+FROM orders o
+JOIN customers c
+	ON o.customer_id = c.customer_id
+LEFT JOIN shippers sh
+	ON o.shipper_id = sh.shipper_id
+JOIN order_statuses os
+	ON o.status = os.order_status_id;
+    
+
+-- Self Outer Joins
+
+USE sql_hr;
+
+
