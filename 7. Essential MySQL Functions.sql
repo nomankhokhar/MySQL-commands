b0303f58ecf0dc7dc6c826b2116e3a232c4eb3b2
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-03-09 04:46:33 +0500

    Functions

--- 7. Essential MySQL Functions.sql
+++ 7. Essential MySQL Functions.sql
@@ -22,4 +22,33 @@ SELECT RTRIM('Kkkkk    ');
 SELECT LEFT('Noman Ali', 4);
 SELECT RIGHT('KHAN SAHIB', 3);
 SELECT SUBSTRING('Ma a Hitler Fan', 5,6); --  Start , length
-SELECT 
+SELECT LOCATE('n' , 'Khan Bhai LOL'); -- return length of the char or string
+SELECT LOCATE('garten', 'BigGarten'); -- Start from 4th index ans will be 4 -> key , string
+SELECT REPLACE('BigGarten', 'garten', 'garden'); -- String , word, replac by word
+SELECT CONCAT('Noman' , 'Ali');
+
+
+USE sql_store;
+
+SELECT CONCAT(first_name, ' ' , last_name) as fullname
+FROM customers;
+
+
+-- Dates Functions
+
+ 
+SELECT NOW(), CURDATE(), CURTIME();
+SELECT YEAR(NOW());
+SELECT HOUR(NOW());
+SELECT DAY(NOW());
+
+-- Date function can be get from Documentation's
+
+SELECT EXTRACT(YEAR FROM NOW());
+
+
+-- Exercise Get orders from the Current Date using Builtin fun's
+
+SELECT *
+FROM  orders
+WHERE YEAR(order_date) = YEAR(NOW());
