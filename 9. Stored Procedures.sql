055a90c6882298cce69eb140cfab8888b4076587
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-03-10 00:02:42 +0500

    Module functions done

--- 9. Stored Procedures.sql
+++ 9. Stored Procedures.sql
@@ -311,3 +311,12 @@ SELECT
     name,
     get_risk_factor_for_client(client_id) as risk_factor
 FROM clients;
+
+
+
+-- Other Convertions
+
+-- fn, procGetRiskFactor, 
+-- getRiskFactor
+-- get_risk_factor
+-- DELIMITER //
