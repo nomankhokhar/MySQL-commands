0bc3cabe6a1cbc9b98bc6b23eba2e0463a80c82c
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-03-09 05:13:11 +0500

    Case Operator Done

--- 7. Essential MySQL Functions.sql
+++ 7. Essential MySQL Functions.sql
@@ -52,3 +52,65 @@ SELECT EXTRACT(YEAR FROM NOW());
 SELECT *
 FROM  orders
 WHERE YEAR(order_date) = YEAR(NOW());
+
+USE sql_store;
+
+-- IFNULL vs COALESCE
+
+SELECT 
+	order_id,
+    IFNULL(shipper_id , 'Not Assign') as shipper
+FROM orders;
+
+
+
+SELECT 
+	order_id,
+    COALESCE(shipper_id , comments ,'Not Assign') as shipper -- If shiiper_id is nullthen comments if comments null then  'Not Assign' 
+FROM orders;
+
+
+-- Exercise Customers , phone
+
+SELECT
+	CONCAT(first_name, ' ' , last_name) AS customer,
+    COALESCE(phone, 'Unknow') AS phone
+FROM customers;
+
+
+
+-- IF Functions
+-- IF(expression , first, second);
+
+SELECT 
+	order_id,
+    order_date,
+    IF(
+    YEAR(order_date) = YEAR(NOW()) ,
+    'Active' , 
+    'Archive') AS Category
+FROM orders;
+
+
+-- Exercise product_id, name, orders,  frequency
+
+SELECT 
+	product_id,
+    name,
+    IF(COUNT(*) > 1, 'Many times', 'Once') AS frequency
+FROM products
+JOIN order_items USING (product_id)
+GROUP BY product_id , name;
+
+
+
+-- CASE Operator
+
+SELECT 
+	order_id,
+    CASE
+		WHEN YEAR(order_date) = YEAR(NOW()) THEN 'Active'
+        WHEN YEAR(order_date) = YEAR(NOW()) - 1 THEN 'Last Year'
+        ELSE 'Future'
+	END as Status
+FROM orders;
