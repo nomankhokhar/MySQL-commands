2990f4ddd0384af01fca397283f1d3ff4bbe44b2
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-03-09 23:20:48 +0500

    Store Parameter Validation Done

--- 9. Stored Procedures.sql
+++ 9. Stored Procedures.sql
@@ -122,6 +122,10 @@ CALL get_clients_by_state(NULL);
 
 -- Store Procedure IF state is NULL with Default Parameters
 
+
+DROP PROCEDURE get_clients_by_state;
+
+
 DELIMITER $$
 CREATE PROCEDURE get_clients_by_state
 (
@@ -138,5 +142,75 @@ END $$
 DELIMITER ;
 
 
+-- Best way to right the Query
+
+DELIMITER $$
+CREATE PROCEDURE get_clients_by_state
+(
+	state CHAR(2)
+)
+BEGIN
+	SELECT * FROM clients c
+	WHERE c.state = IFNULL(state, c.state);
+END $$
+DELIMITER ;
+
+
 CALL get_clients_by_state(null);
 CALL get_clients_by_state('CA');
+
+
+-- Example Write a Stored proceduer called get_payments
+-- with two parameters
+-- client => INT(4)
+-- payment_method_id => TINYINT (1) 0-255 type Only
+
+
+
+DELIMITER $$
+CREATE PROCEDURE get_payments
+(
+	client_id INT,
+    payment_method_id TINYINT
+)
+BEGIN 
+	SELECT *
+    FROM payments p
+    WHERE 
+		p.client_id = IFNULL(client_id, p.client_id) AND
+        p.payment_method = IFNULL(payment_method_id, p.payment_method);
+END $$
+DELIMITER ;
+
+
+CALL get_payments(null , null);
+CALL get_payments(null , 2);
+
+
+-- Parameters Validation of Store Procedure
+
+DROP PROCEDURE make_payment;
+
+DELIMITER $$
+CREATE PROCEDURE make_payment
+(
+	invoice_id INT,
+    payment_amount DECIMAL(9, 2),
+    payment_date DATE
+)
+BEGIN
+	IF payment_amount <= 0 THEN
+     SIGNAL SQLSTATE '22003'
+		SET MESSAGE_TEXT = 'Invalid payment amount';
+    END IF;    
+	UPDATE invoices i
+    SET 
+		i.payment_total = payment_amount,
+        i.payment_date = payment_date
+	WHERE i.invoice_id = invoice_id;
+END $$
+
+DELIMITER ;
+
+
+CALL make_payment(2, -100 , '2019-01-01')
