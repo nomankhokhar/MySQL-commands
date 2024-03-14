80aa5432e486c2acfad2cdbe9719581c80e86f99
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-03-15 03:10:26 +0500

    Best Phase started of Learning

---
+++ 13. Database Designing.sql
@@ -0,0 +1,11 @@
+-- First Step is Data Modeling
+  -- |
+  -- v
+
+-- Understand the requirements
+-- Build a Conceptual Model
+-- Build a Logical Model
+-- Build a Physical Model
+
+
+-- Conceptual Models in MySQL
