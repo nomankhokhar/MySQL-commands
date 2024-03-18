d813a33fbe31e6fe38d41dccc9844d555a23974b
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-03-18 09:35:30 +0500

    Add Points to the Data

--- 14. Indexing for High Performance.sql
+++ 14. Indexing for High Performance.sql
@@ -1,1 +1,9 @@
 -- Indexing for High Performances
+-- Indexes speed up the Queries
+
+-- COST OF INDEXES
+	-- Increase the database
+    -- Slow down the writes
+
+-- For the Above reason we Reserve indexes for performance critical queries
+-- Design indexes based on your queries not your tables.
