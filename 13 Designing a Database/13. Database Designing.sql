6d530f68f2d44584cd506902790f76d7cac723f0
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-03-18 08:53:19 +0500

    Alter COmmand in MySQL

--- 13 Designing a Database/13. Database Designing.sql
+++ 13 Designing a Database/13. Database Designing.sql
@@ -42,3 +42,43 @@
 
 -- Logical Model of Flight Booking System
 -- Logical Model of Rent Video Booking System
+
+
+-- Creating and Droping Database
+
+CREATE DATABASE sql_store2;
+CREATE DATABASE IF NOT EXISTS sql_store2;
+
+
+DROP DATABASE sql_store2;
+DROP DATABASE IF EXISTS sql_store2;
+
+
+-- Create Tables in MySQL
+
+USE sql_store2;
+
+DROP TABLE IF EXISTS customers;
+CREATE TABLE IF NOT EXISTS customers
+(
+	customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name  VARCHAR(50) NOT NULL,
+    points      INT NOT NULL DEFAULT 0,
+    email 		VARCHAR(255) NOT NULL UNIQUE
+);
+
+show tables;
+
+
+-- Altering Tables in MySQL
+
+-- If you have space in column name then use backSpace
+ALTER TABLE customers 
+	ADD last_name VARCHAR(50) NOT NULL AFTER `first_name`,
+    ADD city      VARCHAR(50) NOT NULL,
+    MODIFY COLUMN first_name VARCHAR(55) DEFAULT '',
+    DROP points
+;
+    
+    
+describe customers;
